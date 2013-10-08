378a95afbd77d6fd318f41c1d7eade9930b54b72
mart2967 <mart2967@morris.umn.edu>
2013-10-07 19:42:26 -0500

    fixed parser to parse columns instead of rows

--- AlgorithmsLab5/src/JobMatcher.java
+++ AlgorithmsLab5/src/JobMatcher.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,8 +17,42 @@ public class JobMatcher {
         System.out.println(companies);
         System.out.println(programmers);
 
+        String pairings = makePairs(companies, programmers);
+        System.out.println(pairings);
+    }
 
+    public static String makePairs(ArrayList<ArrayList<Character>> companies, ArrayList<ArrayList<Character>> programmers) {
+        char[][] pairings = new char[companies.size()][2];
+        HashMap<Character, Character> pairs = new HashMap<Character, Character>();
+        for (ArrayList<Character> col : programmers) {
+            pairs.put(col.get(0), null);
+        }
+        for ( int c = 0; c < companies.size(); c++) {
+            char prog = companies.get(c).get(1); // highest ranked programmer of company c
+            System.out.println(prog);
+            if (pairs.get(prog) == null) {
+                pairs.put(prog, companies.get(c).get(0));
+            } else {
+                if (programmers.get(prog-1).indexOf(c+49) > pairs.get(prog)) {
+                    pairs.put(prog, companies.get(c).get(0));
+                }
+            }
+        }
+        System.out.println(pairs);
+        return convertToString(pairs);
+    }
 
+   
+
+    private static String convertToString(Map in) {
+        String output = "";
+        Iterator it = in.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            output += (pairs.getKey() + "" + pairs.getValue() + " ");
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        return output;
     }
 
     public static ArrayList<ArrayList<Character>> parseInput(String filename, boolean company) throws FileNotFoundException {
@@ -25,18 +62,25 @@ public class JobMatcher {
         while(s.hasNextLine()) {
             String line = s.nextLine();
             String[] elements = line.split(" ");
-            ArrayList<Character> column = new ArrayList<Character>();
+            //ArrayList<Character> column = new ArrayList<Character>();
+            
             if (company) {
                 for(int col = 0; col < elements.length/2; col++) {
-                    column.add(elements[col].charAt(0));
+                    if (output.size() <= col) {
+                        output.add(new ArrayList<Character>());
+                    }
+                    output.get(col).add(elements[col].charAt(0));
                 }
 
             } else {
                 for(int col = elements.length/2; col < elements.length; col++) {
-                    column.add(elements[col].charAt(0));
+                    if (output.size() <= col - elements.length/2) {
+                        output.add(new ArrayList<Character>());
+                    }
+                    output.get(col - elements.length/2).add(elements[col].charAt(0));
                 }
             }
-            output.add(column);
+            
             row++;
         }
         s.close();
